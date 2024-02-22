3182efa8c836c17adec7e239b027f719d15bff65
elmin412 <115942958+elmin412@users.noreply.github.com>
2024-02-22 18:51:23 +0100

    first-push

---
+++ src/js/render-functions.js
@@ -0,0 +1,7 @@
+// Описаний у документації
+import SimpleLightbox from "simplelightbox";
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+console.log("hello render")
---
+++ src/main.js
@@ -0,0 +1,6 @@
+// Описаний у документації
+import iziToast from "izitoast";
+// Додатковий імпорт стилів
+import "izitoast/dist/css/iziToast.min.css";
+
+console.log("hello main")
