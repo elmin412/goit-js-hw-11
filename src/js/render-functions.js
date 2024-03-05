f158f705f552801b84a1028c66114b0a88120601
elmin412 <115942958+elmin412@users.noreply.github.com>
2024-03-05 18:06:51 +0100

    push

--- src/js/render-functions.js
+++ src/js/render-functions.js
@@ -1,7 +1,18 @@
-// Описаний у документації
-import SimpleLightbox from "simplelightbox";
-// Додатковий імпорт стилів
-import "simplelightbox/dist/simple-lightbox.min.css";
+export function renderImages(images) {
+    const container = document.querySelector(".gallery");
+    const markup = images.map(({ webformatURL, largeImageURL, tags, likes, views, comments, downloads }) => {
+            return `<li class="gallery-item">
+                <a class="gallery-link" href="${largeImageURL}">
+                <img class="gallery-image"
+                src="${webformatURL}"
+                data-source="${largeImageURL}"
+                alt="${tags}"/></a>
+                <p>likes="${likes}"</p>
+                <p>views="${views}"</p>
+                <p>comments="${comments}"</p>
+                <p>download="${downloads}"</p>
+            </li>`;
+    }).join('')
+      container.insertAdjacentHTML("beforeend", markup);
+}
 
-
-console.log("hello render")
